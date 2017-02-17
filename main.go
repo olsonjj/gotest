b37f46638b738eadcef9779cae57d9b43218479b
John Olson <john.olson2@aristocrat.com>
2017-02-17 15:06:47 -0800

    added jwt example

--- main.go
+++ main.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"time"
+
+	"github.com/robbert229/jwt"
+)
 
 func main() {
 	fmt.Printf("Hello world\n")
+
+	secret := "ThisIsMySuperDuperSecret"
+
+	algor := jwt.HmacSha256(secret)
+
+	claims := jwt.NewClaim()
+	claims.Set("Role", "Admin")
+	claims.SetTime("exp", time.Now().Add(time.Minute))
+
+	token, err := algor.Encode(claims)
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Token: %s\n", token)
+
 }
